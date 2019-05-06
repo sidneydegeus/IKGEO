4f4f7f0977251a96d3a97d670463d90252821a66
Sidney de Geus <sidneydegeus@hotmail.com>
2019-05-06 15:41:22 +0200

    forgot to commit this. Was working on a slider.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ var map;
 var allMarkers = new Map();
 var displayedMarkers = new Map();
 
+var oldestYear;
+var newestYear;
+
 function initMap() {
     // Map options
     var options = {
@@ -14,21 +17,34 @@ function initMap() {
         rotateControl: false,
         fullscreenControl: false,
         styles: [
-            {"featureType": "road.highway",elementType: "labels",stylers:[{visibility: "off"}]}, //turns off highway labels
-            {"featureType": "road.arterial",elementType: "labels",stylers: [{visibility: "off"}]}, //turns off arterial roads labels
-            {"featureType": "road.local",elementType: "labels",stylers: [{visibility: "off"}]}  //turns off local roads labels
+            { "featureType": "road.highway", elementType: "labels", stylers: [{ visibility: "off" }] }, //turns off highway labels
+            { "featureType": "road.arterial", elementType: "labels", stylers: [{ visibility: "off" }] }, //turns off arterial roads labels
+            { "featureType": "road.local", elementType: "labels", stylers: [{ visibility: "off" }] }  //turns off local roads labels
         ]
     };
 
     // New map
     map = new google.maps.Map(document.getElementById('map'), options);
+}
 
-    $.getJSON( "DisasterDeclarationsSummaries-v1.json", function( data ) {
+function LoadData(callback) {
+    $.getJSON("DisasterDeclarationsSummaries-v1.json", function (data) {
         allMarkers = CreateMarkerDictionary(data);
         for (var [key, value] of allMarkers) {
-            DisplayFilter(key);
+            CreateFilter(key);
             displayedMarkers.set(key, []);
         }
+        callback();
+    });
+}
+
+function CreateYearSlider() {
+    // min, max both need to be dynamic... callback isn't working
+    // use static values for now
+    $('#input').slider({
+        min: 1953,
+        max: 2019,
+        range: true
     });
 }
 
@@ -59,27 +75,40 @@ function DisplayMarker(props) {
     return marker;
 }
 
-function DisplayFilter(name) {
+function CreateFilter(name) {
     $('.filter').append(
         '<div class="filter-default ' + name + '">' +
-            '<input type="checkbox" name="checkbox ' + name + '" id="checkbox ' + name + '" value="' + name + '"/>' +
-            '<label class="filterLabel" for="checkbox ' + name + '">' + name + '</label>' +
+        '<input type="checkbox" name="checkbox ' + name + '" id="checkbox ' + name + '" value="' + name + '"/>' +
+        '<label class="filterLabel" for="checkbox ' + name + '">' + name + '</label>' +
         '</div>'
     );
 }
 
 function CreateMarkerDictionary(data) {
-    var markers = new Map();  
+    var markers = new Map();
     for (var i = 0; i < data.length; i++) {
         var tmp = data[i];
         if (!markers.has(tmp.incidentType)) {
-            markers.set(tmp.incidentType, []);
+            markers.set(tmp.incidentType, new Map());
         }
+        var date = new Date(tmp.incidentBeginDate);
+        if (!markers.get(tmp.incidentType).has(date.getFullYear())) {
+            markers.get(tmp.incidentType).set(date.getFullYear(), [])
+        }
+        if (newestYear < date.getFullYear()) {
+            newestYear = date.getFullYear();
+        }
+        if (oldestYear > date.getFullYear()) {
+            oldestYear = date.getFullYear();
+        }
+        //var test = $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address='+ tmp.state);
+        //console.log(test);
         //markers.get(tmp.incidentType).push(CreateMarker(tmp));
     }
     return markers;
 }
 
+// mayber emove
 function CreateMarker(data) {
     // do something with iconimage here
     var marker = {
@@ -90,7 +119,7 @@ function CreateMarker(data) {
     return marker;
 }
 
-$(document).on('click', '.filterLabel', function() {
+$(document).on('click', '.filterLabel', function () {
     var value = $(this).prev("input[type='checkbox']").val();
     var checked = $(this).prev("input[type='checkbox']:checked").length > 0;
     var filterData = allMarkers.get(value);
@@ -98,7 +127,7 @@ $(document).on('click', '.filterLabel', function() {
         if (checked) {
             displayedMarkers.get(value)[i].setMap(null);
         } else {
-            displayedMarkers.get(value).push(DisplayMarker(filterData[i]));    
+            displayedMarkers.get(value).push(DisplayMarker(filterData[i]));
         }
     }
     // empty displayed array
@@ -108,6 +137,8 @@ $(document).on('click', '.filterLabel', function() {
 });
 
 initMap();
-
+LoadData(function () {
+    CreateYearSlider();
+});
 
 
